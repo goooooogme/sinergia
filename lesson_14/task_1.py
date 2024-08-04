07fc0315ec9a08ec9a1214cbd282db24ef73178c
e.nefedov <e.nefedov@megar.ru>
2024-08-05 04:56:04 +0500

    add lesson_14

---
+++ lesson_14/task_1.py
@@ -0,0 +1,12 @@
+def print_elements_recursive(my_list, index=0):
+    if index >= len(my_list):
+        print("Конец списка")
+        return
+    
+    print(my_list[index])
+    
+    print_elements_recursive(my_list, index + 1)
+
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+print_elements_recursive(my_list)
